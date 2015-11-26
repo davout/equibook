5956a3ab484b9c94bad520389a4d70df4cc74655
davout <david.francois@webflows.fr>
2015-11-26 15:53:31 +0100

    Wut

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def landing
+    # This doesn't work apparently
     redirect_to(horses_path) if current_user
   end
 
