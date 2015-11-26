71c69b04211fa37525c892771285513836a3ed04
davout <david.francois@webflows.fr>
2015-11-26 20:56:37 +0100

    Facebook auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,11 +15,12 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
+    # If user has the same e-mail we should handle it gracefully (?)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.fb_image = auth.info.image # assuming the user model has an image
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126165216) do
+ActiveRecord::Schema.define(version: 20151126195122) do
 
   create_table "colors", force: :cascade do |t|
     t.string   "name"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20151126165216) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "fb_image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
