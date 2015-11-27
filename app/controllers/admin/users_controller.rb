2cf8779a26c275c7d68701b8f237c3345235771b
davout <david.francois@webflows.fr>
2015-11-27 14:00:42 +0100

    More Facebook

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < Admin::ApplicationController
   active_scaffold :user do |config|
     config.columns = [:email, :admin, :created_at, :provider, :uid, :fb_image]
-    config.list.columns = [:email, :admin, :created_at, :provider]
+    config.list.columns = [:email, :admin, :created_at, :provider, :fb_image]
     config.update.columns = config.create.columns = [:email, :admin]
     config.show.columns = [:email, :admin, :created_at, :provider, :uid, :fb_image]
     config.nested.add_link(:horses)
--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,7 @@
 module Admin::UsersHelper
+
+  def fb_image_column(user, _column)
+    user.fb_image && image_tag(user.fb_image, title: user.name, alt: user.name, class: 'fb_avatar')
+  end
+
 end
