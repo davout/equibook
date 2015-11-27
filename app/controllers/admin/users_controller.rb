f961b560cb5d2f23adf0d3edcacb92ad3328bd81
davout <david.francois@webflows.fr>
2015-11-27 09:28:05 +0100

    Misc.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 class Admin::UsersController < Admin::ApplicationController
   active_scaffold :user do |config|
     config.columns = [:email, :admin, :created_at, :provider, :uid, :fb_image]
+    config.list.columns = [:email, :admin, :created_at, :provider]
     config.update.columns = config.create.columns = [:email, :admin]
     config.show.columns = [:email, :admin, :created_at, :provider, :uid, :fb_image]
   end
